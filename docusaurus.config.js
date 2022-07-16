74f4e404c4caae7bfc3bbb1e111c38b1dce7e273
shenxin <wb_shenxin@meituan.com>
2022-07-16 16:39:54 +0800

    1

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://botsheng.github.io',
+  url: 'https://shenxin',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
